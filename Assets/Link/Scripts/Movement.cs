83aa9a2765f1d77fe96e7e70514a5e4be42f46a1
Umut-Canakoglu <148465244+Umut-Canakoglu@users.noreply.github.com>
2024-09-30 23:30:21 +0300

    Fixed animation errors

--- Assets/Link/Scripts/Movement.cs
+++ Assets/Link/Scripts/Movement.cs
@@ -6,21 +6,21 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed = 2f;
     [SerializeField] private Rigidbody2D rb;
-    private float horizaontal;
+    private float horizontal;
     private float vertical;
 
     public Animator animator;
     
     void Update()
     {
-        horizaontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        if (horizaontal != 0 && vertical != 0){
+        if (horizontal != 0 && vertical != 0){
             if(rb.velocity.x != 0){
                 vertical = 0;
             }
             else if(rb.velocity.y != 0){
-                horizaontal = 0;
+                horizontal = 0;
             }
         }
         FixedUpdate();
@@ -29,10 +29,16 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizaontal * speed, vertical * speed);
-
-        animator.SetFloat("xSpeed", horizaontal);
-        animator.SetFloat("ySpeed", vertical);
-
+        float beforeHorizontal = rb.velocity.x / speed;
+        float beforeVertical = rb.velocity.y / speed;
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        if (beforeHorizontal != horizontal)
+        {
+            animator.SetFloat("xSpeed", horizontal);
+        } 
+        if (beforeVertical != vertical)
+        {
+            animator.SetFloat("ySpeed", vertical);
+        }
     }
 }
